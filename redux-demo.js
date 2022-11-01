8b3fc9ab3c2130709be87b0b8b1b0fb25429ae94
Emmanuel <ejaramilo@hotmail.com>
2022-11-01 14:19:59 -0500

    231 more redux basics with multiple reducer type actions

--- redux-demo.js
+++ redux-demo.js
@@ -3,9 +3,19 @@ const redux = require('redux');
 // this has to be a pure function, same inputs send the same result, not write a database, 
 // not do a fetch to an API or anything like that
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1
-  };
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -20,4 +30,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: 'increment' });
-store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
