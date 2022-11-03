8bd83fa1fcd2bbedad76eb5885c21fac2e70825f
Emmanuel <ejaramilo@hotmail.com>
2022-11-03 08:38:56 -0500

    242 redux toolkit - adding state slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,27 @@
 import { createStore } from "redux";
+import { createSlice } from '@reduxjs/toolkit';
+// this works as create reducer
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter
+    }
+  }
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
